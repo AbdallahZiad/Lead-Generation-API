15c207881daa0ea6fffb32af7eba600aa673f775
Abdallah Ziad <boodyziad1@gmail.com>
2025-07-07 04:07:17 +0300

    fix headless

--- scrapers/fgas.js
+++ scrapers/fgas.js
@@ -15,7 +15,7 @@ module.exports = async (req, res, next) => {
 
   try {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
